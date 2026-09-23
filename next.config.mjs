996ca1abe1021e2ddb7f2a8fcc5e2b7cff4f6435
felix ongom <felix@ongom.com>
2026-09-23 22:13:29 +0300

    corrected linux error

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,44 @@
 /** @type {import('next').NextConfig} */
 /** @type {import('next').NextConfig} */
+
+/** @type {import('next').NextConfig} */
+
 const nextConfig = {
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-      images: {
-        domains: ['https://stellar-signs.com'], // Add all domains from where you load images
-      },
-       webpack(config) {
-        config.module.rules.push({
-          test: /\.worker\.ts$/,
-          use: { loader: 'worker-loader' },
-        })
-    return config
+  images: {
+    domains: ['stellar-signs.com'],
   },
-    };
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.worker\.ts$/,
+      use: {
+        loader: 'worker-loader',
+      },
+    });
+
+    return config;
+  },
+};
+
 export default nextConfig;
+
+// const nextConfig = {
+//       images: {
+//         domains: ['https://stellar-signs.com'], // Add all domains from where you load images
+//       },
+//        webpack(config) {
+//         config.module.rules.push({
+//           test: /\.worker\.ts$/,
+//           use: { loader: 'worker-loader' },
+//         })
+//     return config
+//   },
+//     };
+// export default nextConfig;
 
 // module.exports = nextConfig;
 // module.exports = nextConfig
